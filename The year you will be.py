ad62d6bac3ceaca89fdd69ea0624acf5ebabdb5d
Void <oliver2069iq@gmail.com>
2025-10-07 21:19:22 +0330

    Add files via upload

---
+++ The year you will be.py
@@ -0,0 +1,8 @@
+
+year = int(input("The year you will be: "))
+
+date = int(input("What year is today? "))
+
+age = int(input("Your age is "))
+
+print("you will be",year,"in the year of",year - age + date,"\nyou will turn",year,year - age,'years later')

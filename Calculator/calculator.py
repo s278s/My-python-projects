58fb0e660af1f587964859ca37cf8a8c01f9d7dd
Oblivion X <oliver2069iq@gmail.com>
2025-11-23 21:33:12 +0330

    Update calculator.py
    This program is safer and more reliable
    All operations +  - * / are destined with separate functions
    Countdown on exit added which makes it better
    and finally Basic error handling

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,11 +1,53 @@
-from decimal import Decimal as D
-from fractions import Fraction as F
+from decimal import Decimal
+from time import sleep
 
-num1 = D(input("Enter a number: "))
-num2 = D(input("Enter another number: "))
+def add(x,y):
+    return x+y
 
-print("%.2f + %.2f = %.2f " % (num1, num2, num1 + num2))
-print("%.2f - %.2f = %.2f " % (num1, num2, num1 - num2))
-print("%.2f * %.2f = %.2f " % (num1, num2, num1 * num2))
-print("%.2f / %.2f = %.2f " % (num1, num2, num1 / num2))
-print('num1 = %s' % F(num1),"\nnum2 = %s" % F(num2))
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
+
+def div(x,y):
+    if y == 0:
+        return 'zero is not allowed'
+    else:
+        return x/y
+
+
+while True:
+    operator = input('enter one of \n+ - * / exit: ')
+    if operator.lower() == 'exit':
+        for i in range(3,0,-1):
+            print(i,end='..' if i >= 2 else '\n')
+            sleep(1)
+        break
+
+    if operator not in '+-*/':
+        print('wrong operator')
+        continue
+
+    try:
+        number1 = Decimal(input('enter the first number: '))
+        number2 = Decimal(input('enter the second number: '))
+    except:
+        print('wrong number')
+        continue
+
+    if operator == '+':
+        print(add(number1,number2))
+        continue
+
+    if operator == '-':
+        print(sub(number1,number2))
+        continue
+
+    if operator == '*':
+        print(mul(number1,number2))
+        continue
+
+    if operator == '/':
+        print(div(number1,number2))
+print('You exited the program')
